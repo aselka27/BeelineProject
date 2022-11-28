f44f237563806ba978d8296c4f2096b10bf1bae0
aselka27 <asjumalieva@gmail.com>
2022-11-29 00:40:11 +0600

    Initial Commit

---
+++ BeelineProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeelineProject
+//
+//  Created by саргашкаева on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
